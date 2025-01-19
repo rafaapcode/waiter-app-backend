e6a4f35c429518c40af177c15de5a64e1899096d
rafaapcode <rafaap2003@gmail.com>
2025-01-19 13:34:06 -0300

    feat: implementing the CONFIG module to read the environment varibles and creating the constant file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,11 +1,10 @@
 import * as mongoose from 'mongoose';
+import { CONSTANTS } from 'src/constants';
 
 export const databaseProviders = [
   {
-    provide: 'DATABASE_CONNECTION',
+    provide: CONSTANTS.DATABASE_CONNECTION,
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(
-        'mongodb+srv://rafaapcode:fDpLsH6Gdr6VpiPO@cluster0.6k11s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-      ),
+      mongoose.connect(process.env.DATABASE_URI),
   },
 ];
