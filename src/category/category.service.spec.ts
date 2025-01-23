c3ec079740cabcd1b4320a3ac4dc2389e08c5392
rafaapcode <rafaap2003@gmail.com>
2025-01-23 08:44:08 -0300

    feat: configuring the tests

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CategoryRepository } from 'src/repository/category/category.service';
 import { CategoryService } from './category.service';
 
 describe('CategoryService', () => {
   let service: CategoryService;
+  let repo: CategoryRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [CategoryService, CategoryRepository],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
+    repo = module.get<CategoryRepository>(CategoryRepository);
   });
 
-  it('should be defined', () => {
+  it('should service be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should repo be defined', () => {
+    expect(repo).toBeDefined();
   });
 });
--- src/order/order.service.spec.ts
+++ src/order/order.service.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { OrderRepository } from 'src/repository/order/order.service';
 import { OrderService } from './order.service';
 
 describe('OrderService', () => {
   let service: OrderService;
+  let order: OrderRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderService],
+      providers: [OrderService, OrderRepository],
     }).compile();
 
     service = module.get<OrderService>(OrderService);
+    order = module.get<OrderRepository>(OrderRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should orde be defined', () => {
+    expect(order).toBeDefined();
   });
 });
