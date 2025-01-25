494445f45e68fdb136a1d472b4253a9193697459
rafaapcode <rafaap2003@gmail.com>
2025-01-25 11:04:11 -0300

    feat: using zod to validate instead of class-validator

--- src/product/dto/Product.dto.ts
+++ src/product/dto/Product.dto.ts
@@ -1,29 +1,28 @@
 import { z } from 'zod';
 
 export const createProductSchema = z.object({
-  product: z.object({
-    name: z.string({ message: 'Nome é obrigatório' }).min(3, {
-      message: 'Nome é obrigatório e deve ter ao menos 3 caracteres',
-    }),
-    description: z.string({ message: 'Descrição é obrigatório' }).min(10, {
-      message: 'Descrição é obrigatório e deve ter ao menos 10 caracteres',
-    }),
-    imageUrl: z
-      .string({ message: 'ImageUrl é obrigatório' })
-      .regex(/[^(http?|https?|ftp):\/\/[^\s/$.?#].[^\s]*$]/, {
-        message: 'URL inválida',
-      }),
-    price: z.number({ message: 'Preço é obrigatório' }),
-    ingredients: z.array(
-      z.object({
-        name: z.string({ message: 'Nome é obrigatório' }),
-        icon: z.string().optional(),
-      }),
-    ),
-    category: z
-      .string({ message: 'PRODUCT deve conter um ID válido' })
-      .length(24, { message: 'ID invalido' }),
+  name: z.string({ message: 'Nome é obrigatório' }).min(3, {
+    message: 'Nome é obrigatório e deve ter ao menos 3 caracteres',
   }),
+  description: z.string({ message: 'Descrição é obrigatório' }).min(10, {
+    message: 'Descrição é obrigatório e deve ter ao menos 10 caracteres',
+  }),
+  imageUrl: z
+    .string({ message: 'ImageUrl é obrigatório' })
+    .regex(/[^(http?|https?|ftp):\/\/[^\s/$.?#].[^\s]*$]/, {
+      message: 'URL inválida',
+    })
+    .optional(),
+  price: z.number({ message: 'Preço é obrigatório' }),
+  ingredients: z.array(
+    z.object({
+      name: z.string({ message: 'Nome é obrigatório' }),
+      icon: z.string().optional(),
+    }),
+  ),
+  category: z
+    .string({ message: 'PRODUCT deve conter um ID válido' })
+    .length(24, { message: 'ID invalido' }),
 });
 
 export type CreateProductDTO = z.infer<typeof createProductSchema>;
