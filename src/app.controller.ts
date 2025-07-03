b45a7da3be006090ec71646de0efed9c0a6ba91b
rafaapcode <rafaap2003@gmail.com>
2025-07-03 07:52:59 -0300

    feat: made the health endpoint public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { IsPublic } from '@shared/decorators/isPublic';
 
+@IsPublic()
 @Controller('health')
 export class AppController {
   constructor() {}
